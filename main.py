bb89f0bb53e8dee6c585222c2b69a219f562ed83
Rhea Rai <rhearai.tech@gmail.com>
2024-10-07 10:13:16 -0500

    created a new branch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import shutil
 import sys
 from argparse import ArgumentParser
 from collections import deque
-import time 
+import time
 import h5py
 import tqdm
 from matplotlib import cm
@@ -28,68 +28,75 @@ import hydra
 
 MODEL_DICT = {'single_step': SingleStep, 'multi_step': MultiStep}
 
+
 def load_dataset(dataset_path):
     dataset = h5py.File(dataset_path, "r")
     dataset_keys = []
     dataset.visit(lambda key: dataset_keys.append(key) if isinstance(dataset[key], h5py.Dataset) else None)
-    
+
     mem_dataset = {}
     for key in dataset_keys:
         mem_dataset[key] = dataset[key][:]
     dataset = mem_dataset
-    
+
     obs_shape = dataset["obs"][0].shape
     act_shape = dataset["action"].max() + 1
     return dataset, obs_shape, act_shape
+
 
 def create_models(cfg: DictConfig, obs_shape, act_shape):
     algo_cfgs = cfg.algos
     model_names = list(algo_cfgs.keys())
     models = {}
-    
+
     for model_name in model_names:
         model_cfg = algo_cfgs[model_name]
         model = MODEL_DICT[model_name](obs_shape=obs_shape, act_shape=act_shape, encoder_cfg=cfg.encoder, forward_cfg=cfg.forward, inverse_cfg=cfg.inverse, **model_cfg,)
-        models[model_name] = model
+        models[model_name] = model #TODO: save the pretrained ssi model here
     return models
-        
+
+
 def initialize_dependant_models(models):
     for model_name, model in models.items():
         model.share_dependant_models(models)
     return models
 
+
 def log_to_wandb(cfg, models, logs, samples, train_step):
     if train_step % cfg.met_log_freq == 0:
-        labeled_logs = {f"{algo_name}/{key}": value for algo_name, algo_log in logs.items() for key, value in algo_log.items()}
+        labeled_logs = {f"{algo_name}/{key}": value for algo_name,
+                        algo_log in logs.items() for key, value in algo_log.items()}
         wandb.log(labeled_logs, step=train_step)
     if train_step % cfg.img_log_freq == 0:
         for model_name, model in models.items():
             obs = samples["obs"][0]
-            img = wandb.Image(np.swapaxes(perturb_heatmap(obs, model.encoder)[1], 0,2))
+            img = wandb.Image(np.swapaxes(
+                perturb_heatmap(obs, model.encoder)[1], 0, 2))
             wandb.log({f"{model_name}/heatmap": img}, step=train_step)
+
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: DictConfig):
     log_path = cfg.logdir + ("_" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-        
-    if cfg.wandb: 
-        wandb.init(entity='maxrudolph', project="nav2d", config={})
-        
+
+    if cfg.wandb:
+        wandb.init(entity='rhearai', project="nav2d", config={})
+
     random.seed(cfg.seed)
     torch.manual_seed(cfg.seed)
-    
+
     dataset, obs_shape, act_shape = load_dataset(cfg.dataset)
     models = create_models(cfg, obs_shape, act_shape)
     models = initialize_dependant_models(models)
-    
+
     train(cfg, dataset, models)
-    
-    
+
+
 def train(cfg: DictConfig, dataset, models):
     dataset_keys = list(dataset.keys())
     wandb_logs = {key: {} for key in models.keys()}
     train_step = 0
-    
+
     for epoch in range(cfg.n_epochs):
         sample_ind_all = np.random.permutation(len(dataset["obs"]))
         sample_ind_next = np.random.permutation(len(dataset["obs"]))
@@ -102,15 +109,12 @@ def train(cfg: DictConfig, dataset, models):
             for model_name, model in models.items():
                 log = model.train_step(samples, epoch)
                 wandb_logs[model_name].update(log)
-                
+
             if cfg.wandb:
                 log_to_wandb(cfg, models, wandb_logs, samples, train_step)
-                
-            train_step += 1            
-            
 
-                
-            
+            train_step += 1
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
