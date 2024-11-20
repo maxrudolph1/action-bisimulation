1c8a6394385444a6ce6e3923676c6e2e139d6919
Rhea Rai <rhearai.tech@gmail.com>
2024-11-20 07:13:21 -0600

    finished last frequency test for now, wandb log 37

--- representations/multi_step.py
+++ representations/multi_step.py
@@ -85,6 +85,204 @@ class MultiStep(torch.nn.Module):
         if epoch < self.ss_train_warmup_epochs:
             return {}
 
+        
+        obs_seq = torch.as_tensor(batch["kobs"], device="cuda")
+        act_seq = torch.as_tensor(batch["kaction"], device="cuda")
+        k_steps = obs_seq.shape[1]-1 #-1 for the obs we start on
+
+        cumalative_fw_loss = 0
+
+        '''
+        # obs_x = torch.as_tensor(batch["obs"], device="cuda")  # observation ( x )
+        # obs_x_next = torch.as_tensor(batch["obs_next"], device="cuda") # next observation ( x' )
+        # act = torch.as_tensor(batch["action"], device="cuda") # action taken at time step ( a_x )
+
+        # if train_step % self.reset_forward_model_every == 0 and self.reset_forward_model_every > 0:
+        #     self.forward_model.reset_weights()
+
+        # # we shuffle the observations to get a random other observation, may need to change this later because the sampling is biased.
+        # comp_idxs = np.random.permutation(np.arange(obs_x.shape[0]))
+        # obs_y = obs_x[comp_idxs]
+        # obs_y_next = obs_x_next[comp_idxs]
+
+
+        # bs = obs_x.shape[0]
+
+        ## NOT SUPPORTING K-step forward modeling right now
+        # kobs_steps = torch.randint(0, self.k_steps-1, (bs,)).numpy().tolist()
+        # obs_y = torch.as_tensor(batch["kobs"], device="cuda")[torch.arange(bs), kobs_steps, :,:,:] # random other observation ( y )
+        # kobs_x = torch.as_tensor(batch["kobs"], device="cuda")[torch.arange(bs), self.k_steps_dyn, :,:,:] # obsveration k_steps_dyn away
+        # kact_x = torch.as_tensor(batch["kaction"], device="cuda") # action taken at time step ( a_x )
+
+
+        # if self.use_states_with_same_action:
+        #     act_x = act
+        #     possible_actions = [0,1,2,3]
+        #     similar_action_obs = obs_x.clone()
+        #     similar_action_obs_next = obs_x_next.clone()
+        #     for action in possible_actions:
+        #         cur_act_idx = torch.nonzero(act_x == action)
+        #         shuffled_actions = torch.randperm(len(cur_act_idx))
+        #         similar_action_obs[cur_act_idx] = obs_x[cur_act_idx[shuffled_actions]]
+        #         similar_action_obs_next[cur_act_idx] = obs_x_next[cur_act_idx[shuffled_actions]]
+        #     obs_y = similar_action_obs
+        #     obs_y_next = similar_action_obs_next
+        '''
+
+        #encode first obs
+        ox_encoded_online = self.encoder(obs_seq[:,0]) 
+        # oy_encoded_online = self.encoder(obs_y)
+        with torch.no_grad():
+            ox_encoded_target = self.target_encoder(obs_seq[:,0]) # target encoder z = \hat{phi}(x)
+            ss_encoded_x = self.ss_encoder(obs_seq[:,0])
+            
+            # oy_encoded_target = self.target_encoder(obs_y) # target encoder z = \hat{phi}(y)
+            # oxk_encoded_target = self.target_encoder(kobs_x)  # target encoder z = \hat{phi}( x^(k) )
+            # oxn_encoded_target = self.target_encoder(obs_x_next) # target encoder z = \hat{phi}( x' )
+
+        curr_latent_state = ox_encoded_online.detach()
+
+        for k in range(0, k_steps):
+            act_k = act_seq[:,k]
+            latent_forward_prediction = self.forward_model(curr_latent_state, act_k)
+            
+            #encoding the next target state?
+            with torch.no_grad():
+                target_latent_state = self.target_encoder(obs_seq[:,k+1])
+
+            forward_model_next_loss = F.smooth_l1_loss(
+                latent_forward_prediction,
+                target_latent_state.detach(),
+            )
+
+            cumalative_fw_loss += forward_model_next_loss
+            curr_latent_state = latent_forward_prediction
+
+
+        #post k steps:
+        with torch.no_grad():
+            oy_encoded_target = self.target_encoder(obs_seq[:,-1]) 
+            ss_encoded_y = self.ss_encoder(obs_seq[:,-1])
+            ss_diffs = (ss_encoded_x - ss_encoded_y)
+            ss_distances = torch.linalg.norm(ss_diffs, ord=1, dim=-1)
+
+
+        oy_encoded_online = self.encoder(obs_seq[:,-1])
+        ox_oy_diffs = ox_encoded_online - oy_encoded_online
+        distances = torch.linalg.norm(ox_oy_diffs, ord=1, dim=-1) #distance btwn online encoder output?
+
+        #target distance btwn predicted & target latenedt states
+        target_distances = torch.linalg.norm(curr_latent_state - oy_encoded_target, ord=1, dim=-1)
+
+
+        #this is the cumalative multistep loss
+        ms_loss = F.smooth_l1_loss(
+            distances, (1 - self.gamma) * ss_distances.detach() + self.gamma * target_distances.detach()
+        )
+
+        self.encoder_optimizer.zero_grad()
+        self.forward_model_optimizer.zero_grad()
+        ms_loss.backward()
+        cumalative_fw_loss.backward()
+        self.encoder_optimizer.step()
+        self.forward_model_optimizer.step() 
+
+
+        if self.steps_until_sync == 0:
+            self._sync_params()
+            self.steps_until_sync = self.sync_freq
+        else:
+            self.steps_until_sync -= 1
+
+        log = {
+            "total_loss": ms_loss.detach().item(),
+            "base_case_distance": ss_distances.float().mean().detach().item(),
+            "cur_ms_distance": distances.float().mean().detach().item(),
+            "forward_ms_distance": target_distances.float().mean().detach().item(),
+            "forward_model_loss": cumalative_fw_loss.detach().item(),
+            "gamma": self.gamma,
+        }
+        return log
+
+        # # optimize latent forward model
+        # for _ in range(self.forward_model_steps_per_batch):
+        #     latent_forward_prediction = self.forward_model(ox_encoded_target.detach(), act)
+
+        #     forward_model_next_loss = F.smooth_l1_loss(
+        #         latent_forward_prediction,
+        #         oxn_encoded_target.detach(),
+        #     )
+        #     self.forward_model_optimizer.zero_grad()
+        #     forward_model_next_loss.backward()
+        #     self.forward_model_optimizer.step()
+
+        # with torch.no_grad():
+        #     ss_encoded_x = self.ss_encoder(obs_x)
+        #     ss_encoded_y = self.ss_encoder(obs_y)
+        #     ss_diffs = (ss_encoded_x - ss_encoded_y).detach()
+        #     ss_distances = torch.linalg.norm(ss_diffs, ord=1, dim=-1)
+
+        #     if self.use_states_with_same_action:
+        #         pred_ox_encoded = self.encoder(obs_x_next).unsqueeze(1)
+        #         pred_oy_encoded = self.encoder(obs_y_next).unsqueeze(1)
+        #     else:
+        #         pred_ox_encoded = self.forward_model(
+        #             ox_encoded_target.unsqueeze(1).expand(-1, self.act_shape, -1),
+        #             torch.arange(self.act_shape, device="cuda")
+        #             .unsqueeze(0)
+        #             .expand(ox_encoded_target.shape[0], -1),
+        #         )  # shape (n, 4, e)
+        #         # pred_ox_encoded = self.forward_model(ox_encoded_target, act)
+        #         # pred_oy_encoded = self.forward_model(oy_encoded_target, act)
+        #         pred_oy_encoded = self.forward_model(
+        #             oy_encoded_target.unsqueeze(1).expand(-1, self.act_shape, -1),
+        #             torch.arange(self.act_shape, device="cuda")
+        #             .unsqueeze(0)
+        #             .expand(ox_encoded_target.shape[0], -1),
+        #         )  # shape (n, 4, e)
+
+
+        #     target_distances = torch.linalg.norm(
+        #         pred_ox_encoded - pred_oy_encoded, ord=1, dim=-1
+        #     )
+        #     target_distances = torch.mean(target_distances, dim=-1)
+
+        # distances = torch.linalg.norm(ox_encoded_online - oy_encoded_online, ord=1, dim=-1)
+
+        # target_ms_distance_size = target_distances.float().mean().detach().item()
+        # cur_ms_distance_size = distances.float().mean().detach().item()
+
+
+
+        # ms_loss = F.smooth_l1_loss(
+        #     distances, (1 - self.gamma) * ss_distances.detach() + self.gamma * target_distances.detach()
+        # )
+
+        # self.encoder_optimizer.zero_grad()
+        # ms_loss.backward()
+        # self.encoder_optimizer.step()
+
+        # if self.steps_until_sync == 0:
+        #     self._sync_params()
+        #     self.steps_until_sync = self.sync_freq
+        # else:
+        #     self.steps_until_sync -= 1
+
+
+        # log = {
+        #     "total_loss": ms_loss.detach().item(),
+        #     "base_case_distance": ss_distances.float().mean().detach().item(),
+        #     "cur_ms_distance": cur_ms_distance_size,
+        #     "forward_ms_distance": target_ms_distance_size,
+        #     "gamma": self.gamma,
+        #     "forward_model_loss": forward_model_next_loss.detach().item(),
+        # }
+        # return log
+    
+    def old_train_step(self, batch, epoch, train_step):
+        if epoch < self.ss_train_warmup_epochs:
+            return {}
+
         obs_x = torch.as_tensor(batch["obs"], device="cuda")  # observation ( x )
         obs_x_next = torch.as_tensor(batch["obs_next"], device="cuda") # next observation ( x' )
         act = torch.as_tensor(batch["action"], device="cuda") # action taken at time step ( a_x )
