484df532fe3262f4dfca82f86884f9152b70072f
Evan Kuo <evan.kuo.edu@gmail.com>
2025-07-16 00:00:32 +0000

    update examine script

--- exorl_dm_control/examine_exorl_dataset.py
+++ exorl_dm_control/examine_exorl_dataset.py
@@ -8,14 +8,14 @@ def examine_episode(episode_path):
     print(f"Keys: {list(data.keys())}")
 
     for key in data.keys():
-        print(f"  {key}: shape={data[key].shape}, dtype={data[key].dtype}")
-        if key == 'observation':
-            print(f"    Sample observation: {data[key][0]}")
+        print(f"\n  {key}: shape={data[key].shape}, dtype={data[key].dtype}")
+        print(f" ---Sample {key}: {data[key][10]}")
     print()
 
 # Examine first few episodes
 buffer_dir = "~/bisim/exorl/datasets/point_mass_maze/rnd/buffer"
-episodes = sorted(glob.glob(os.path.expanduser(f"{buffer_dir}/*.npz")))
+# episodes = sorted(glob.glob(os.path.expanduser(f"{buffer_dir}/*.npz")))
+episodes = glob.glob(os.path.expanduser(f"{buffer_dir}/*.npz"))
 
 for ep in episodes[:2]:
     examine_episode(ep)
